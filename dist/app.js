6b97e7aff0a0bf24d675ee39371ae56e5d274c47
Konsek90 <48626081+Konsek90@users.noreply.github.com>
2020-06-12 23:52:30 +0200

    Stworzenie klas odpowiedzialnych za pola

--- dist/app.js
+++ dist/app.js
@@ -14,8 +14,83 @@ var FieldLabel = /** @class */ (function () {
     FieldLabel.stworz = function (pole) {
         var etykieta = document.createElement("label");
         etykieta.innerHTML = pole.etykieta;
-        etykieta.htmlFor = pole.nazwa;
+        etykieta.htmlFor = pole.element.name;
         return etykieta;
     };
     return FieldLabel;
+}());
+var InputField = /** @class */ (function () {
+    function InputField(nazwa, etykieta, typ) {
+        this.etykieta = etykieta;
+        this.element = document.createElement("input");
+        this.element.name = nazwa;
+        this.element.type = this.typ = typ;
+    }
+    InputField.prototype.render = function (rodzic) {
+        var labelElement = FieldLabel.stworz(this);
+        rodzic.appendChild(labelElement);
+        rodzic.appendChild(this.element);
+    };
+    InputField.prototype.getValue = function () {
+        return this.element.value;
+    };
+    return InputField;
+}());
+var TextAreaField = /** @class */ (function () {
+    function TextAreaField(nazwa, etykieta) {
+        this.typ = FieldType.TEXTAREA;
+        this.etykieta = etykieta;
+        this.element = document.createElement("textarea");
+        this.element.name = nazwa;
+    }
+    TextAreaField.prototype.render = function (rodzic) {
+        var labelElement = FieldLabel.stworz(this);
+        rodzic.appendChild(labelElement);
+        rodzic.appendChild(this.element);
+    };
+    TextAreaField.prototype.getValue = function () {
+        return this.element.value;
+    };
+    return TextAreaField;
+}());
+var SelectField = /** @class */ (function () {
+    function SelectField(nazwa, etykieta, wartosci) {
+        this.typ = FieldType.TEXTAREA;
+        this.etykieta = etykieta;
+        this.element = document.createElement("select");
+        this.element.name = nazwa;
+        for (var _i = 0, wartosci_1 = wartosci; _i < wartosci_1.length; _i++) {
+            var wartosc = wartosci_1[_i];
+            var optionElement = document.createElement("option");
+            optionElement.text = optionElement.value = wartosc;
+            this.element.appendChild(optionElement);
+        }
+    }
+    SelectField.prototype.render = function (rodzic) {
+        var labelElement = FieldLabel.stworz(this);
+        rodzic.appendChild(labelElement);
+        rodzic.appendChild(this.element);
+    };
+    SelectField.prototype.getValue = function () {
+        return this.element.value;
+    };
+    return SelectField;
+}());
+var CheckboxField = /** @class */ (function () {
+    function CheckboxField(nazwa, etykieta) {
+        this.typ = FieldType.CHECKBOX;
+        this.etykieta = etykieta;
+        this.element = document.createElement("input");
+        this.element.name = nazwa;
+        this.element.type = this.typ;
+    }
+    CheckboxField.prototype.render = function (rodzic) {
+        var labelElement = FieldLabel.stworz(this);
+        rodzic.appendChild(labelElement);
+        rodzic.appendChild(this.element);
+    };
+    CheckboxField.prototype.getValue = function () {
+        return this.element.value;
+    };
+    return CheckboxField;
 }());
--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ enum FieldType {
 }
 
 interface Field {
-    nazwa: string,
+    element: HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement,
     etykieta: string,
     typ: FieldType,
     render(rodzic: HTMLElement): void,
@@ -19,7 +19,103 @@ class FieldLabel {
     static stworz(pole: Field): HTMLLabelElement {
         let etykieta = document.createElement("label");
         etykieta.innerHTML = pole.etykieta;
-        etykieta.htmlFor = pole.nazwa;
+        etykieta.htmlFor = pole.element.name;
         return etykieta;
+    }
+}
+
+class InputField implements Field {
+    element: HTMLInputElement;
+    etykieta: string;
+    typ: FieldType;
+
+    constructor(nazwa: string, etykieta: string, typ: FieldType) {
+        this.etykieta = etykieta;
+        this.element = document.createElement("input");
+        this.element.name = nazwa;
+        this.element.type = this.typ = typ;
+    }
+
+    render(rodzic: HTMLElement): void {
+        let labelElement = FieldLabel.stworz(this) as HTMLLabelElement;
+        rodzic.appendChild(labelElement);
+        rodzic.appendChild(this.element);
+    }
+
+    getValue(): string {
+        return this.element.value;
+    }
+}
+
+class TextAreaField implements Field {
+    element: HTMLTextAreaElement;
+    etykieta: string;
+    typ = FieldType.TEXTAREA;
+
+    constructor(nazwa: string, etykieta: string) {
+        this.etykieta = etykieta;
+        this.element = document.createElement("textarea");
+        this.element.name = nazwa;
+    }
+
+    render(rodzic: HTMLElement): void {
+        let labelElement = FieldLabel.stworz(this) as HTMLLabelElement;
+        rodzic.appendChild(labelElement);
+        rodzic.appendChild(this.element);
+    }
+
+    getValue(): string {
+        return this.element.value;
+    }
+}
+
+class SelectField implements Field {
+    element: HTMLSelectElement;
+    etykieta: string;
+    typ = FieldType.TEXTAREA;
+
+    constructor(nazwa: string, etykieta: string, wartosci: string[]) {
+        this.etykieta = etykieta;
+        this.element = document.createElement("select");
+        this.element.name = nazwa;
+
+        for (const wartosc of wartosci) {
+            let optionElement = document.createElement("option");
+            optionElement.text = optionElement.value = wartosc;
+            this.element.appendChild(optionElement);
+        }
+    }
+
+    render(rodzic: HTMLElement): void {
+        let labelElement = FieldLabel.stworz(this) as HTMLLabelElement;
+        rodzic.appendChild(labelElement);
+        rodzic.appendChild(this.element);
+    }
+
+    getValue(): string {
+        return this.element.value;
+    }
+}
+
+class CheckboxField implements Field {
+    element: HTMLInputElement;
+    etykieta: string;
+    typ = FieldType.CHECKBOX;
+
+    constructor(nazwa: string, etykieta: string) {
+        this.etykieta = etykieta;
+        this.element = document.createElement("input");
+        this.element.name = nazwa;
+        this.element.type = this.typ;
+    }
+
+    render(rodzic: HTMLElement): void {
+        let labelElement = FieldLabel.stworz(this) as HTMLLabelElement;
+        rodzic.appendChild(labelElement);
+        rodzic.appendChild(this.element);
+    }
+
+    getValue(): string {
+        return this.element.value;
     }
 }
